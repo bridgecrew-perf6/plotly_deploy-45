37e7734cb78a49c7976692e863c0c53800109a12
zxiaoxi <z.xiaoxi@live.ca>
2022-01-30 20:55:00 -0800

    Complete one chart

--- charts.js
+++ charts.js
@@ -58,30 +58,50 @@ function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file 
   d3.json("samples.json").then((data) => {
     // 3. Create a variable that holds the samples array. 
-
+    var samples = data.samples;
     // 4. Create a variable that filters the samples for the object with the desired sample number.
-
+    var resultArray = samples.filter(sampleObj => sampleObj.id == sample);
     //  5. Create a variable that holds the first sample in the array.
-
+    var result = resultArray[0];
+    console.log(result);
 
     // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
+    var otu_ids = result.otu_ids;
 
+    var otu_labels = result.otu_labels;
+
+    var sample_values = result.sample_values;
+    console.log(sample_values);
 
     // 7. Create the yticks for the bar chart.
     // Hint: Get the the top 10 otu_ids and map them in descending order  
-    //  so the otu_ids with the most bacteria are last. 
+    //  so the otu_ids with the most bacteria are last
+    var sorted_samples = sample_values.sort((a,b) => a.sample_values - b.sample_values);
+    console.log(sorted_samples);
 
-    var yticks = 
+    var top_10_samples = sorted_samples.slice(0,9);
+    console.log(top_10_samples);
+
+    var otu = "OTU ";
+    var yticks = otu_ids.map(otu_ids => otu.concat(otu_ids)).slice(0,9);
+    console.log(yticks);
 
     // 8. Create the trace for the bar chart. 
-    var barData = [
-      
-    ];
+    var barData = {
+      x: top_10_samples,
+      y: yticks,
+      text: otu_labels,
+      orientation: 'h',
+      type: 'bar'
+    };
     // 9. Create the layout for the bar chart. 
     var barLayout = {
-     
+      title: "Top 10 Bacteria Cultures Found",
+      xaxis: {title: ""},
+      yaxis: {title: ""}
     };
     // 10. Use Plotly to plot the data with the layout. 
     
+    Plotly.newPlot("bar", [barData], barLayout);
   });
 }
